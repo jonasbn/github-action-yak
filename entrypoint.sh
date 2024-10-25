5d55717e740b9b6f968703f1a6879788f1670244
jonasbn <jonasbn@gmail.com>
2024-10-25 18:13:31 +0200

    Addressed some warnings from shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -l
 
-echo ""
-echo "Executing yak with the following arguments: "
-echo "---------------------------------------------"
-echo "yak --noconfig --nochecksums $@"
-echo "---------------------------------------------"
-echo ""
+echo ''
+echo 'Executing yak with the following arguments: '
+echo '---------------------------------------------'
+echo 'yak --noconfig --nochecksums $@'
+echo '---------------------------------------------'
+echo ''
 
 PATH=$PATH:/usr/src/app/script
 
-yak --noconfig --nochecksums $@
+yak --noconfig --nochecksums "$@"
 
 EXITCODE=$?
 
